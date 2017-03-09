dcd6eb6f0901b16748d6363cef117f520d0c118c
viperior <viperior@gmail.com>
2017-03-09 11:28:17 +0000

    Update readme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # lucky-search
 Grabs the top result URL for a series of search terms.
+
+Google Geocoding API Documentation:
+https://developers.google.com/maps/documentation/geocoding/start
+
+Nokogiri HTML Parsing Tutorial:
+http://ruby.bastardsbook.com/chapters/html-parsing/
--- geocoding-api-endpoint.rb
+++ geocoding-api-endpoint.rb
@@ -3,16 +3,8 @@ class GeocodingAPIEndpoint
   attr_reader :uri
 
   def initialize(address, api_key)
-    #@api_key = URIParameter.new('key', api_key)
-    #@address = URIParameter.new('address', address)
-    #@options = []
-    #@options.push(@address)
-    #@options.push(@api_key)
-    #@uri = LuckyURI.new('https://maps.googleapis.com/maps/api/geocode/json', @options).uri
     address = URI.parse( URI.encode( address ) ).to_s
-    pp address
     @uri = "https://maps.googleapis.com/maps/api/geocode/json?address=#{address}&key=#{api_key}"
-    pp @uri
   end
 
 end
